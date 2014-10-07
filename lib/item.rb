b951b2dca2f969402572f823738815a77910a001
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-10-07 13:16:17 -0600

    add item and item_test

---
+++ lib/item.rb
@@ -0,0 +1,19 @@
+class Item
+	attr_reader :id,
+							:name,
+							:description,
+							:unit_price,
+							:merchant_id,
+							:created_at,
+							:updated_at
+
+	def initialize(attributes)
+		@id 					= attributes[:id]
+		@name 				= attributes[:name]
+		@description 	= attributes[:description]
+		@merchant_id 	= attributes[:merchant_id]
+		@unit_price 	= attributes[:unit_price]
+		@updated_at 	= attributes[:updated_at]
+		@created_at 	= attributes[:created_at]
+	end
+end
---
+++ test/item_test.rb
@@ -0,0 +1,28 @@
+require_relative 'test_helper'
+require_relative '../lib/item'
+
+class ItemTest < Minitest::Test
+	def test_it_exists
+		assert Item
+	end
+
+	def test_it_assigns_correct_attributes
+		attributes = { id: "1",
+									 name: "item1",
+									 description: "here is a description",
+									 unit_price: "2.99",
+									 merchant_id: "2",
+									 created_at: "2012-03-27 14:53:59 UTC",
+									 updated_at: "2012-03-27 14:53:59 UTC" }
+
+		item       = Item.new(attributes)
+
+		assert_equal "1", item.id
+		assert_equal "item1", item.name
+		assert_equal "here is a description", item.description
+		assert_equal "2.99", item.unit_price
+		assert_equal "2", item.merchant_id
+		assert_equal "2012-03-27 14:53:59 UTC", item.updated_at
+		assert_equal "2012-03-27 14:53:59 UTC", item.created_at
+	end
+end
